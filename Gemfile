c14f36c496e78c1dd0eec64a44016dec53e016ce
Takashi Abe <tabe@tabe-ltm.internal.salesforce.com>
2016-03-22 11:08:28 +0900

    Changed correct redirect page

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'restforce', '2.1.2'
 gem 'omniauth-salesforce'
 gem 'slim-rails'
 
+gem 'thin'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.2'
 # Use sqlite3 as the database for Active Record
--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,26 @@
 class TestController < ApplicationController
+  def create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to request.env['omniauth.origin'] || root_url
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
   def index
+    if current_user
+      client = Restforce.new :oauth_token => current_user.oauth_token,
+        :refresh_token => current_user.refresh_token,
+        :instance_url  => current_user.instance_url
+
+  #        :client_id     => Rails.application.config.salesforce_app_id,
+  #        :client_secret => Rails.application.config.salesforce_app_secret
+
+      accounts = client.query("select Id, Name from Account")
+      @account = accounts.first
+    end
   end
 end
